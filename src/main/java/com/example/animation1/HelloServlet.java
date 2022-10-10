8948c759f903eb4985078b276ac4943319d70836
Quang Hà <btkkentxx2002@gmail.com>
2022-10-10 13:46:45 +0700

    animation

---
+++ src/main/java/com/example/animation1/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.example.animation1;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+    public void destroy() {
+    }
+}
